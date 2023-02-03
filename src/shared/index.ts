040baee732b4dbe60f40d4e9e07f90ddaae451da
Semenov <semenov@mail.ru>
2023-02-03 09:28:39 +0400

    добавлены импорты shared - store, model, dom

---
+++ src/shared/index.ts
@@ -0,0 +1,9 @@
+import * as Dom from "@shared/dom/index";
+import * as Model from "@shared/model/index";
+import * as Store from "@shared/store/index";
+
+export {
+    Dom,
+    Model,
+    Store
+}
