80cf86d61594b7113c7fdcaa8e4a58a319c750aa
Semenov <semenov@mail.ru>
2023-02-05 14:53:36 +0400

    добавлены импорты shared - libs/array

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,9 +1,11 @@
 import * as Dom from "@shared/dom/index";
 import * as Model from "@shared/model/index";
 import * as Store from "@shared/store/index";
+import * as Libs from "@shared/libs";
 
 export {
     Dom,
     Model,
-    Store
+    Store,
+    Libs
 }
