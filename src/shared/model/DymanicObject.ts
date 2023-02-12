892a297a6c5865c99383f619a60c8201d533d12b
Semenov <semenov@mail.ru>
2023-02-12 13:35:23 +0400

    разработка shared model - DymanicObject, add import shared DymanicObject

---
+++ src/shared/model/DymanicObject.ts
@@ -0,0 +1,1 @@
+export type DymanicObject<T> = { [key: string]: T };
--- src/shared/model/index.ts
+++ src/shared/model/index.ts
@@ -1,2 +1,3 @@
 export type { ObjectAny } from  "@shared/model/ObjectAny";
 export type { DymanicNull } from  "@shared/model/DymanicNull";
+export type { DymanicObject } from  "@shared/model/DymanicObject";
